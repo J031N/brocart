24c35300cb250b0f2cc3f4bb48b6a43ba68f3037
Jobin <jobinj2512@gmail.com>
2024-05-03 15:59:12 +0530

    procfile file added

--- brocart/settings.py
+++ brocart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,8 @@ MEDIA_ROOT=os.path.join( BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Activate django heroku
+
+django_heroku.settings(locals())
