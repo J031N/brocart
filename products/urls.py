189e07d5d52902222c2ddd379865cc8093799e33
Jobin <jobinj2512@gmail.com>
2024-05-03 09:58:37 +0530

    requirements.txt added

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from products import views
 
 urlpatterns = [
     path('home/',views.index,name='home'),
+    path('',views.index,name='home'),
     path('products/',views.list_products,name='products'),
     path('product_details/<id>',views.details_product,name='product_details'),
    
